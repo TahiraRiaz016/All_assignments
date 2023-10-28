762520bcd64f08eb99d77adea8e7ad2c944f038f
Tahira Riaz <134882055+TahiraRiaz016@users.noreply.github.com>
2023-10-29 01:56:07 +0500

    Add files via upload

---
+++ TahiraRiaz-Assignment#   13/For loop Assignment.js
@@ -0,0 +1,96 @@
+
+
+// Q1) Create a program that prints the numbers from 1 to 10.
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+
+// Q2) Print the numbers from 10 down to 1 in reverse order.
+
+
+//   for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+//   }
+
+// Q3) Print the elements of an array using a for loop with their index number: [ 'apple',
+// 'banana', 'cherry', 'date' ].
+
+
+// let fruits = ['apple', 'banana', 'cherry', 'date'];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(`${i} ${fruits[i]}`);
+// }
+
+// Q4) Print the table of 2 in correct order from 2 to 10 using a for loop.
+
+// let table = 2;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${table} * ${i} = ${table * i} `);
+// }
+
+// Q5) Print the table of 2 in reverse order from 10 to 1 using a for loop.
+
+// let table = 2;
+// for (let i = 10; i >= 1; i--) {
+//   console.log(`${table} * ${i} = ${table * i}`);
+// }
+
+// Q6) Calculate the sum of numbers from 1 to 5 using a for loop.
+
+ 
+// let sum = 0;
+
+// for (let i = 1; i <= 5; i++) {
+//     sum += i;
+// }
+
+// console.log(`Sum: ${sum}`);
+
+
+// Q7) Print the even numbers from 2 to 10. (Hint: use if else to find the even numbers with
+//     for loop)
+// === ka matlab hai even number 
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+ 
+// ! ka matla hai odd number 
+
+// Q8) Print the odd numbers from 1 to 10. (Hint: use if else to find the odd numbers with for
+//     loop)
+
+
+// for (let num = 1; num <= 10; num++) {
+//     if (num % 2 !== 0) {
+//       console.log(num);
+//     }
+//   }
+  
+// Q9) Create a program that prints the squares of numbers from 1 to 5.
+
+
+// for (let i = 1; i <= 5; i++) {
+//     let square = i * i;
+//     console.log(square);
+// }
+
+
+// Q10) Count the number of even numbers from 1 to 20 using a for loop.
+
+
+// let cont = 0;
+
+// for (let i = 1; i <= 20; i++) 
+// {
+//     if (i % 2 === 0) {
+//         cont++;
+//     }
+// }
+
+// console.log(`numbers: ${cont}`);
---
+++ TahiraRiaz-Assignment#   13/practiceForLoops.js
@@ -0,0 +1,42 @@
+
+
+// JavaScript For Loop syntax 
+
+// initialization condition increment/decrement
+
+// for(let a = 1; a <= 10; a++){
+//     document.write("Tahira Riaz")
+// }
+
+// for(let a = 1; a <=10; a ++)
+// {
+// document.write("Tahira <br>");
+// }
+// document.write("End of For loop")
+
+
+// for(let a = 10; a <=1; a ++)
+// {
+// document.write("Tahira <br>");
+// }
+// console.log("End of For loop")
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let i = 1 ; i < 11; i++) {
+//     console.log(i + ". sorry");
+    
+// }
+
+// for (let i = 1 ; i <= 10; i++) {
+//     console.log(i + ". Tahira");
+    
+// }
+
+// for (let i = 10 ; i > 0 ; i--) {
+//     console.log(i + ". Tahira");
+    
+// }
