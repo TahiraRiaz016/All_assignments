3b3895cd9e27ae27b27a3c79432204c31666974a
Tahira Riaz <134882055+TahiraRiaz016@users.noreply.github.com>
2023-11-12 02:51:03 +0500

    Add files via upload

---
+++ Tahira_assessment_01/apps.js
@@ -0,0 +1,147 @@
+// for(let i = 1; i <= 5; i++){
+
+//     console.log("Great");
+
+//     }
+    
+
+
+//     let numbers = [1,2, 3, 4, 6];
+// let abc = 0;
+// for (let i = 0; i < 5; i++) {
+// abc += numbers[i];
+// }
+// console.log(abc);
+    
+
+// let userInput = prompt("Enter a number:");
+
+
+// let number = parseFloat(userInput);
+
+
+// if (isNaN(number)) {
+//     console.log("Invalid input. Please enter a valid number.");
+
+// } else if (number < 10) {
+//     console.log("Less than 10");
+
+// } else if (number > 10) {
+//     console.log("Greater than 10");
+
+// }
+
+
+
+// let userInput = prompt("Enter a number:");
+
+
+// let number = parseInt(userInput);
+
+
+// if (isNaN(number)) {  
+//     console.log("Invalid input. Please enter a valid number.");
+
+// } else if (number % 2 === 0) {
+//     console.log("Even");
+
+
+// } else {
+//     console.log("Odd");
+// }
+
+
+// NaN means >> Not a Number
+
+
+// let input1 = prompt("Enter the first number:");
+// let number1 = parseFloat(input1); 
+
+
+// let input2 = prompt("Enter the second number:");
+// let number2 = parseFloat(input2); 
+
+
+// if (isNaN(number1) || isNaN(number2)) {
+//     console.log("Invalid input. Please enter valid numbers.");
+// } else {
+    
+
+//     let sum = number1 + number2;
+//     console.log("Sum is " + sum);
+// }
+
+
+
+// let Array = ['banana', 'cherry'];
+
+
+// Array.push('apple');
+
+// console.log(Array);
+
+
+
+
+
+// let fruits = ['apple', 'banana', 'cherry'];
+
+
+// fruits.pop();
+
+
+// console.log(fruits);
+
+
+
+// var fruits = ['apple', 'banana', 'cherry'];
+
+
+// fruits.unshift('orange');
+
+
+// console.log(fruits);
+
+
+
+
+
+// let fruits = ['orange', 'apple', 'banana', 'cherry'];
+
+// fruits.shift();
+
+// console.log(fruits);
+
+
+
+// let fruits = ['apple', 'banana', 'cherry'];
+
+// fruits.splice(1, 1);
+
+// console.log(fruits);
+
+
+
+// let fruits = ['apple', 'banana', 'cherry'];
+
+// fruits.reverse();
+
+// console.log(fruits);
+
+
+
+// let fruits = ["apple", "banana", "orange", "strawberry"];
+
+
+// fruits.forEach(function(fruit) {
+//     console.log(fruit);
+// });
+
+
+
+
+// let fruits = ['apple', 'banana', 'cherry'];
+
+// fruits.splice(2, 0, 'grape');
+
+// console.log(fruits);
