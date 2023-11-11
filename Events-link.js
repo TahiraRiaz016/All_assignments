602f8c0daa05e4c4d8205d177308d22ea6fa2253
Tahira Riaz <134882055+TahiraRiaz016@users.noreply.github.com>
2023-11-12 02:38:35 +0500

    Add files via upload

---
+++ Events-link.js
@@ -0,0 +1,37 @@
+
+
+// let mybutton = document.getElementsByTagName("button")[0];
+
+// mybutton.addEventListener("click",function(){
+//     console.log("click");
+// })
+
+
+// let mybutton = document.getElementsByTagName("button")[0];
+
+// mybutton.addEventListener("click",() =>{
+//     console.log("click");
+// })
+
+// let mybutton = document.getElementsByTagName("button")[0];
+
+// mybutton.addEventListener("click",function(){
+//     console.log("Hello");
+// })
+
+
+// let mybutton = document.getElementsByTagName("button")[0];
+
+// mybutton.addEventListener("click",function(){
+//     console.log("Hello ");
+// })
+
+let mybutton = document.getElementsByTagName("button")[0]
+let count = 0;
+
+mybutton.addEventListener("click",function(){
+    
+    count++ 
+
+    console.log(count);
+})
